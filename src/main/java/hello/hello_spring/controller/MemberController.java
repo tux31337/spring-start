0e0f5bafbd5fbf3120c97d7713f32e74fbbd1e40
tux31337 <ljm920604@gmail.com>
2025-01-30 21:59:18 +0900

    Feat. SpringConfig 로 직접 설정하기, DI 주입 변경해보기(Setter 주입, 필드 주입, 생성자 주입)

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -6,9 +6,20 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
+    //필드 주입
+//    @Autowired private final MemberService memberService;
 
+    // setter 주입 : public하게 set 메서드가 있어서 좋지않음, 로딩시점에 바꾸는건지 setting되면 바꿀일이 없음
+//    private final MemberService memberService;
+//
+//    @Autowired
+//    public void setMemberService(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
+
+    // 생성자 주입
     private final MemberService memberService;
-
+    
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
