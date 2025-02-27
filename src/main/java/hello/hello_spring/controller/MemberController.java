6b0592356218f4d31032ca882e5557ba3ea53d00
tux31337 <ljm920604@gmail.com>
2025-02-28 08:55:01 +0900

    Feat. 홈 화면 기능 추가

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
-    //필드 주입
+    //필드 주입 : 중간에 바꿀수 있는 방법없음.
 //    @Autowired private final MemberService memberService;
 
     // setter 주입 : public하게 set 메서드가 있어서 좋지않음, 로딩시점에 바꾸는건지 setting되면 바꿀일이 없음
