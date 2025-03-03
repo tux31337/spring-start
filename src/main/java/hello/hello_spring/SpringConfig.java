2156677a76da79d50dbf462cb86b6c7a6d6e76b3
JJONG\ljm17 <ljm920604@gmail.com>
2025-03-03 16:01:05 +0900

    Feat. JPA

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -11,18 +11,13 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    private final MemberRepository memberRepository;
 
-    public SpringConfig(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
-
-//    private EntityManager em;
-//
-//    @Autowired
-//    public SpringConfig(EntityManager em) {
-//        this.em = em;
-//    }
 
 //    private DataSource dataSource;
 //
@@ -33,14 +28,14 @@ public class SpringConfig {
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository);
+        return new MemberService(memberRepository());
     }
-//
-//    @Bean
-//    public MemberRepository memberRepository() {
-//        //return  new MemoryMemberRepository();
-//        // return new JdbcMemberRepository(dataSource);
-//        // return new JdbcTemplateMemberRepository(dataSource);
-//        return new JpaMemberRepository(em);
-//    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        //return  new MemoryMemberRepository();
+        // return new JdbcMemberRepository(dataSource);
+        // return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
+    }
 }
--- src/main/java/hello/hello_spring/domain/Member.java
+++ src/main/java/hello/hello_spring/domain/Member.java
@@ -2,13 +2,15 @@ package hello.hello_spring.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Member {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 
     public Long getId() {
