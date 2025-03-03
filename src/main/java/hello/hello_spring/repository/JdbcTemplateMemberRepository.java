f5f1ebe590bcf537a491bb5c775041180eb7dc23
tux31337 <ljm920604@gmail.com>
2025-03-03 09:58:10 +0900

    Feat. jdbcTemplate 완료

--- src/main/java/hello/hello_spring/repository/JdbcTemplateMemberRepository.java
+++ src/main/java/hello/hello_spring/repository/JdbcTemplateMemberRepository.java
@@ -1,35 +1,32 @@
 package hello.hello_spring.repository;
 
 import hello.hello_spring.domain.Member;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
-public class JdbcTemplateMemberRepository implements MemberRepository{
-
+public class JdbcTemplateMemberRepository implements MemberRepository {
     private final JdbcTemplate jdbcTemplate;
 
-    @Autowired
     public JdbcTemplateMemberRepository(DataSource dataSource) {
-        jdbcTemplate = new JdbcTemplate(dataSource)
+        jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     @Override
     public Member save(Member member) {
-        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert("jdbcTemplate");
-        jdbcInsert.withTableName("member").usingGeneratedKeyColumns("id");
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("member")
+                .usingGeneratedKeyColumns("id");
 
-        HashMap<String, Object> parameters = new HashMap<>();
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("name", member.getName());
 
         Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
         member.setId(key.longValue());
@@ -38,29 +35,35 @@ public class JdbcTemplateMemberRepository implements MemberRepository{
 
     @Override
     public Optional<Member> findById(Long id) {
-        List<Member> result = jdbcTemplate.query("select * from member where id = ?", memberRowMapper());
+        List<Member> result = jdbcTemplate.query(
+                "SELECT * FROM member WHERE id = ?",
+                memberRowMapper(),
+                id
+        );
         return result.stream().findAny();
     }
 
     @Override
-    public Optional<Member> findByName(String name) {
-        return Optional.empty();
+    public List<Member> findAll() {
+        return jdbcTemplate.query("SELECT * FROM member", memberRowMapper());
     }
 
     @Override
-    public List<Member> findAll() {
-        return List.of();
+    public Optional<Member> findByName(String name) {
+        List<Member> result = jdbcTemplate.query(
+                "SELECT * FROM member WHERE name = ?",
+                memberRowMapper(),
+                name
+        );
+        return result.stream().findAny();
     }
 
     private RowMapper<Member> memberRowMapper() {
-        return new RowMapper<Member>() {
-            @Override
-            public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
-                Member member = new Member();
-                member.setId(rs.getLong("id"));
-                member.setName(rs.getString("name"));
-                return member;
-            }
-        }
+        return (rs, rowNum) -> {
+            Member member = new Member();
+            member.setId(rs.getLong("id"));
+            member.setName(rs.getString("name"));
+            return member;
+        };
     }
 }
